fc0c41733d721681ea2cd28cb5604024cf950949
Kubrick Code <kubrickcode@gmail.com>
2023-06-22 17:53:57 +0900

    style: 페이드 적용

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,21 +1,27 @@
 import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const Profile = lazy(() => import("./Profile/Profile"));
 const Skills = lazy(() => import("./Skills/Skills"));
 const Projects = lazy(() => import("./Projects/Projects"));
 
 const Router = () => {
+  const location = useLocation();
   return (
     <ErrorBoundary FallbackComponent={<div>Error...</div>}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Profile />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </Suspense>
+      <TransitionGroup>
+        <CSSTransition key={location.pathname} timeout={500} classNames="fade">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Profile />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/projects" element={<Projects />} />
+            </Routes>
+          </Suspense>
+        </CSSTransition>
+      </TransitionGroup>
     </ErrorBoundary>
   );
 };
